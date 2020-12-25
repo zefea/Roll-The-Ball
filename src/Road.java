21ce83a4dc4ff77584e6bb8e381758bac84ce358
Zeynep Ferah Akkurt <46840115+zeynepferahakkurt@users.noreply.github.com>
2020-12-26 01:17:53 +0300

    Rename Road.java to src/Road.java

--- src/Road.java
+++ src/Road.java
@@ -4,7 +4,7 @@
  * Term Project
  * 
  * Zeynep Ferah Akkurt - 150119824
- * Merve Rana Kızıl - 150119825
+ * Merve Rana KÃ½zÃ½l - 150119825
  * 
  * Road.java
  * This class is to check if there is a path from start point to end point. 
